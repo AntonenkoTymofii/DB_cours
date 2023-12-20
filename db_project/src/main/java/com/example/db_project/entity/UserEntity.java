1aa1a18353ee403f1d3c5895acba30fdfb76ace2
AntonenkoTymofii <antonenkotimofii@gmail.com>
2023-12-20 17:44:07 +0200

    add code to entity

---
+++ db_project/src/main/java/com/example/db_project/entity/UserEntity.java
@@ -0,0 +1,68 @@
+package com.example.db_project.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "User")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String nickname;
+    private String email;
+    private String password;
+
+    public UserEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ db_project/src/main/java/com/example/db_project/repository/UserRepo.java
@@ -0,0 +1,4 @@
+package com.example.db_project.repository;
+
+public class UserRepo {
+}
